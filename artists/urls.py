0af0e0567388f5925aa3878868b64f0b3c9e212f
DoWhat <zsibbaszto@gmail.com>
2019-12-30 20:47:29 +0100

    edit artists

--- artists/urls.py
+++ artists/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.artists, name='artists'),
     path('search/', ArtistResults.as_view(), name='artist_search'),
     path('<int:id>/', views.artist_detail, name='artist_detail'),
-
+    path('new_artist', views.new_artist, name='new_artist'),
+    path('edit_artist/<int:id>', views.edit_artist, name='edit_artist'),
 ]

1945936e44ecbbec5bf2ba50727ee4db94239239
DoWhat <zsibbaszto@gmail.com>
2020-01-01 15:09:10 +0100

    add technics delete

--- technics/urls.py
+++ technics/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import TechnicListView, TechnicDetailView
+from .views import TechnicListView, TechnicDetailView, TechnicDeleteView
 
 
 urlpatterns = [
     #path('', views.technics, name='technics'),
     path('<slug:slug>', TechnicDetailView.as_view(), name='technic_detail'),
-    path('', TechnicListView.as_view(), name='technic_list')
-
+    path('', TechnicListView.as_view(), name='technic_list'),
+    path('<slug:slug>/delete/', TechnicDeleteView.as_view(), name='technic_delete'),
 ]
--- technics/views.py
+++ technics/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, DeleteView
+from django.urls import reverse
 
 from .models import Technic
 
@@ -12,3 +14,12 @@ class TechnicDetailView(DetailView):
     model = Technic
     template_name = 'technic_detail.html'
 
+class TechnicDeleteView(DeleteView):
+    template_name = 'technic_delete.html'
+
+    def get_object(self):
+        slug_ = self.kwargs.get("slug")
+        return get_object_or_404(Technic, slug=slug_)
+
+    def get_success_url(self):
+        return reverse('technic_list')

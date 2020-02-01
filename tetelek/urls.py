67d0f2076e2b5958cf46c6f88a61b4d005307dc1
DoWhat <zsibbaszto@gmail.com>
2020-02-01 17:36:54 +0100

    again

--- tetelek/urls.py
+++ tetelek/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('create/', views.create_tetelek, name='create_tetelek'),
     #path('update/<slug:slug>', views.update_lot, name='update_lot'),
     #path('<slug:slug>', views.detail_lot, name='detail_lot'),
-    #path('artist_auto_complete/', views.artist_auto_complete, name='artist_auto_complete'),
+    path('auto_complete/', views.auto_complete, name='auto_complete'),
 
 ]
--- tetelek/views.py
+++ tetelek/views.py
@@ -1,6 +1,10 @@
+import json
+
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Tetelek
+from .models import Tetelek, Artist
 from .forms import TetelekForm
 
 @login_required
@@ -16,3 +20,17 @@ def create_tetelek(request):
         form.save()
     form = TetelekForm()
     return render(request, 'create_tetel.html', {'form': form})
+
+def auto_complete(request):
+    q = request.GET.get('term', '')
+    # users = User.objects.filter(is_active=True)
+    users = Artist.objects.filter(Q(name__icontains=q))
+    users_list = []
+
+    for u in users:
+        value = '%s' % (u.name)
+        u_dict = {'id': u.id, 'label': value}
+        users_list.append(u_dict)
+    data = json.dumps(users_list)
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)

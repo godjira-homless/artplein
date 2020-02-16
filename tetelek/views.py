4b59e64d3dd192b224ea379ffb29f5cb25a22435
zsibbaszto@gmail.com <zsibbaszto@gmail.com>
2020-02-16 10:52:53 +0100

    added extra to tetelek

--- tetelek/views.py
+++ tetelek/views.py
@@ -5,15 +5,17 @@ from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-
+from extra.models import Extras
 from .models import Tetelek, Artist
 from .forms import TetelekForm
+
 
 @login_required
 def tetelek_list(request):
     tetelek = Tetelek.objects.all()
     context = {'items': tetelek}
     return render(request, 'tetelek_list.html', context)
+
 
 @login_required
 def create_tetelek(request):
@@ -26,9 +28,13 @@ def create_tetelek(request):
         return HttpResponseRedirect(reverse('tetelek_list'))
     next_code = Tetelek.objects.last_code()
     next_code.code += 1
-    form = TetelekForm(initial={'code': next_code})
+    extra_artist = Extras.objects.filter(owner=request.user).values_list('artist', flat=True)
+    aid = extra_artist[0]
+    artist_name = Artist.objects.values_list('name', flat=True).get(pk=aid)
+    form = TetelekForm(initial={'code': next_code, 'artist': artist_name})
     # form = TetelekForm()
     return render(request, 'create_tetel.html', {'form': form})
+
 
 @login_required
 def auto_complete(request):
@@ -44,6 +50,7 @@ def auto_complete(request):
     data = json.dumps(users_list)
     mimetype = 'application/json'
     return HttpResponse(data, mimetype)
+
 
 @login_required
 def update_tetel(request, slug):

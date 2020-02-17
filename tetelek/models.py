e4da4ed235237472c2dd286c51911abac34c7567
Kalmár Dániel <daniel.kalmar@moresimp.com>
2020-02-17 13:44:53 +0100

    added images autonumber

--- tetelek/models.py
+++ tetelek/models.py
@@ -11,25 +11,34 @@ from artists.models import Artist
 from django.conf import settings
 
 
+def exist_file(ph):
+    return exists(ph)
+
 def path_and_rename(instance, filename):
     upload_to = 'images/'
-
     ext = filename.split('.')[-1]
-    # get filename
-    if instance.code:
-        filename = '{}.{}'.format(instance.code, ext)
-        ph = "media/images/" + str(filename)
-        if exists(ph):
-            kieg = "_{:02d}".format(1)
-            ext = filename.split('.')[-1]
-            filename = '{}{}.{}'.format(instance.code, kieg, ext)
+    filename = '{}.{}'.format(instance.code, ext)
+    return os.path.join(upload_to, filename)
 
 
+def path_and_rename2(instance, filename):
+    upload_to = 'images/'
+    ext = "jpg"
+    ph = "media/images/{}.{}".format(instance.code, ext)
 
+    if exist_file(ph):
+        kieg_n = 1;
+        kieg = "_{:02d}".format(kieg_n)
+        filename = '{}{}.{}'.format(instance.code, kieg, ext)
+        ph = "media/images/{}".format(filename)
+        if exist_file(ph):
+            while exist_file(ph):
+                kieg_n += 1
+                kieg = "_{:02d}".format(kieg_n)
+                filename = '{}{}.{}'.format(instance.code, kieg, ext)
+                ph = "media/images/{}".format(filename)
     else:
-        # set filename as random string
-        filename = '{}.{}'.format("525", ext)
-    # return the whole path to the file
+        filename = '{}.{}'.format(instance.code, ext)
 
     return os.path.join(upload_to, filename)
 
@@ -50,7 +59,7 @@ class Tetelek(models.Model):
     code = models.IntegerField(blank=False, default=None, unique=True)
     title = models.CharField(max_length=120, blank=False)
     artist = models.ForeignKey(Artist, null=True, blank=True, on_delete=models.SET_NULL)
-    photo = models.ImageField(upload_to=path_and_rename, default='images/default.jpg')
+    photo = models.ImageField(upload_to=path_and_rename2, default='images/default.jpg')
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL,
                                    null=True, blank=True, related_name='created', on_delete=models.SET('1'))
     modified_by = models.ForeignKey(User, null=True, related_name='modified', on_delete=models.SET('1'))
